687bffefd68313578273a28fc0e081ba33847d14
Sebastien Wertz <sebastien.wertz@cern.ch>
2020-05-19 09:14:16 +0200

    run with 2 threads

--- PhysicsTools/NanoAOD/test/topNano_cmsDrivers.sh
+++ PhysicsTools/NanoAOD/test/topNano_cmsDrivers.sh
@@ -1,5 +1,5 @@
 function runDriver () {
-    cmsDriver.py NANO --python_filename topNano_v6p1_$1_cfg.py --fileout file:tree.root -s NANO --mc --conditions $2 --era $3 --eventcontent NANOAODSIM --datatier NANOAODSIM --customise_commands="process.add_(cms.Service('InitRootHandlers', EnableIMT = cms.untracked.bool(False)));process.MessageLogger.cerr.FwkReport.reportEvery=1000;process.NANOAODSIMoutput.fakeNameForCrab=cms.untracked.bool(True)" -n -1 --no_exec
+    cmsDriver.py NANO --python_filename topNano_v6p1_$1_cfg.py --fileout file:tree.root -s NANO --mc --conditions $2 --era $3 --eventcontent NANOAODSIM --datatier NANOAODSIM --customise_commands="process.add_(cms.Service('InitRootHandlers', EnableIMT = cms.untracked.bool(False)));process.MessageLogger.cerr.FwkReport.reportEvery=1000;process.NANOAODSIMoutput.fakeNameForCrab=cms.untracked.bool(True)" --nThreads 2 -n -1 --no_exec
 }
 
 runDriver 2016 102X_mcRun2_asymptotic_v7      Run2_2016,run2_nanoAOD_94X2016
